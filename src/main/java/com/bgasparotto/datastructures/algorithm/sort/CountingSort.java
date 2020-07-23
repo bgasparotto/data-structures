c2d9d60adca41106600892e31822b20d2bbeca79
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-07-23 08:28:01 +0200

    Fixed package name and formatting on the imported example

--- src/main/java/com/bgasparotto/datastructures/algorithm/sort/CountingSort.java
+++ src/main/java/com/bgasparotto/datastructures/algorithm/sort/CountingSort.java
@@ -1,9 +1,9 @@
-package academy.learnprogramming.countingsort;
+package com.bgasparotto.datastructures.algorithm.sort;
 
 public class CountingSort {
 
     public static void main(String[] args) {
-        int[] intArray = { 2, 5, 9, 8, 2, 8, 7, 10, 4, 3 };
+        int[] intArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
 
         countingSort(intArray, 1, 10);
 
