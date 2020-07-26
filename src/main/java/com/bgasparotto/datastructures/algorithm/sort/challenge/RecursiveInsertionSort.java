9288ffbe97eb6586fd4c857a5e0d3ed4dd1a40d4
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-07-26 10:21:30 +0200

    Added the solution of the course's challenge of Insertion Sort with recursion

---
+++ src/main/java/com/bgasparotto/datastructures/algorithm/sort/challenge/RecursiveInsertionSort.java
@@ -0,0 +1,29 @@
+package com.bgasparotto.datastructures.algorithm.sort.challenge;
+
+import static com.bgasparotto.datastructures.algorithm.sort.common.ArrayOperations.print;
+
+public class RecursiveInsertionSort {
+    public static void main(String[] args) {
+        int[] array = {20, 35, -15, 7, 55, 1, -22};
+
+        RecursiveInsertionSort sorter = new RecursiveInsertionSort();
+        sorter.sortFromUnsortedIndex(array, 1);
+        print(array);
+    }
+
+    public void sortFromUnsortedIndex(int[] array, int unsortedIndex) {
+        if (unsortedIndex >= array.length) {
+            return;
+        }
+
+        int newElement = array[unsortedIndex];
+        int j = unsortedIndex - 1;
+        while (j >= 0 && array[j] >= newElement) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = newElement;
+
+        sortFromUnsortedIndex(array, ++unsortedIndex);
+    }
+}
