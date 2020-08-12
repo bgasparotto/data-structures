0338d9ae7614c912c6b86b080c43f31facd3ae52
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-08-12 09:46:33 +0200

    Added a palindrome implementation where the string is logically divided in two for evaluation

---
+++ src/main/java/com/bgasparotto/datastructures/datastructure/stack/challenge/StringDivisionPalindromeEvaluator.java
@@ -0,0 +1,46 @@
+package com.bgasparotto.datastructures.datastructure.stack.challenge;
+
+import com.bgasparotto.datastructures.datastructure.stack.SimpleLinkedListStack;
+import com.bgasparotto.datastructures.datastructure.stack.SimpleStack;
+
+public class StringDivisionPalindromeEvaluator extends PalindromeEvaluator {
+    private static final String LOWER_CASE_ALPHANUMERIC_ONLY_REGEX = "[^a-z0-9]";
+
+    @Override
+    public boolean isPalindrome(String input) {
+        if (isInvalid(input)) {
+            return false;
+        }
+
+        String cleanLowerCaseInput = lowerCaseAlphanumericOnly(input);
+        return inputIsPalindrome(cleanLowerCaseInput);
+    }
+
+    private String lowerCaseAlphanumericOnly(String input) {
+        return input
+                .toLowerCase()
+                .replaceAll(LOWER_CASE_ALPHANUMERIC_ONLY_REGEX, "");
+    }
+
+    private boolean inputIsPalindrome(String input) {
+        int middleEndIndex = input.length() / 2;
+        SimpleStack<Character> stack = new SimpleLinkedListStack<>();
+
+        for (int i = 0; i < middleEndIndex; i++) {
+            char c = input.charAt(i);
+            stack.push(c);
+        }
+
+        int singleMiddleElementLeap = input.length() % 2;
+        int middleStartIndex = middleEndIndex + singleMiddleElementLeap;
+
+        for (int i = middleStartIndex; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/com/bgasparotto/datastructures/datastructure/stack/challenge/StringDivisionPalindromeEvaluatorTest.java
@@ -0,0 +1,8 @@
+package com.bgasparotto.datastructures.datastructure.stack.challenge;
+
+public class StringDivisionPalindromeEvaluatorTest extends AbstractPalindromeEvaluatorTest {
+
+    public StringDivisionPalindromeEvaluatorTest() {
+        super(new StringDivisionPalindromeEvaluator());
+    }
+}
