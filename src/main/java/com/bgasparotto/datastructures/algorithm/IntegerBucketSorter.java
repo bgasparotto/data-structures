792eff1971ad76b37673b2dcb4371b328b89413e
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-09-01 08:43:08 +0200

    Added an example implementation of Bucket Sort for integers with a matching number of digits

---
+++ src/main/java/com/bgasparotto/datastructures/algorithm/IntegerBucketSorter.java
@@ -0,0 +1,56 @@
+package com.bgasparotto.datastructures.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerBucketSorter {
+    public static final int NUMBER_OF_BUCKETS = 10;
+
+    public void sort(int[] input) {
+        var buckets = createSortingBuckets();
+
+        scatterInputIntoBuckets(input, buckets);
+        sortBuckets(buckets);
+        gatherFromBucketsIntoInput(buckets, input);
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Integer>[] createSortingBuckets() {
+        List<Integer>[] buckets = (List<Integer>[]) new List[NUMBER_OF_BUCKETS];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        return buckets;
+    }
+
+    private void scatterInputIntoBuckets(int[] input, List<Integer>[] buckets) {
+        for (int value : input) {
+            int hashedValue = hashValue(value);
+            buckets[hashedValue].add(value);
+        }
+    }
+
+    private int hashValue(int value) {
+        int width = String.valueOf(value).length();
+        return value / (int) Math.pow(10, width - 1);
+    }
+
+    private void sortBuckets(List<Integer>[] buckets) {
+        for (List<Integer> bucket : buckets) {
+            bucket.sort(Integer::compareTo);
+        }
+    }
+
+    private void gatherFromBucketsIntoInput(List<Integer>[] buckets, int[] input) {
+        int inputIndex = 0;
+        int bucketIndex = 0;
+
+        while (inputIndex < input.length) {
+            List<Integer> currentBucket = buckets[bucketIndex++];
+            for (int value : currentBucket) {
+                input[inputIndex++] = value;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/bgasparotto/datastructures/algorithm/IntegerBucketSortTest.java
@@ -0,0 +1,36 @@
+package com.bgasparotto.datastructures.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IntegerBucketSortTest {
+    private final IntegerBucketSorter sorter = new IntegerBucketSorter();
+
+    @Test
+    public void shouldSortOneDigitPositiveIntegerInputArray() {
+        int[] inputOneDigit = {5, 4, 8, 6, 9, 9, 4};
+
+        sorter.sort(inputOneDigit);
+
+        assertThat(inputOneDigit).isEqualTo(new int[]{4, 4, 5, 6, 8, 9, 9});
+    }
+
+    @Test
+    public void shouldSortTwoDigitPositiveIntegerInputArray() {
+        int[] input = {54, 46, 83, 66, 95, 92, 43};
+
+        sorter.sort(input);
+
+        assertThat(input).isEqualTo(new int[]{43, 46, 54, 66, 83, 92, 95});
+    }
+
+    @Test
+    public void shouldSortThreeDigitPositiveIntegerInputArray() {
+        int[] inputThreeDigits = {354, 346, 283, 966, 895, 992, 143};
+
+        sorter.sort(inputThreeDigits);
+
+        assertThat(inputThreeDigits).isEqualTo(new int[]{143, 283, 346, 354, 895, 966, 992});
+    }
+}
