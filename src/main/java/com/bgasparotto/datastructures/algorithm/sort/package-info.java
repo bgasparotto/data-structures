43e0ff27d8473c872f0ae75b0ea9996975da53bd
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-09-27 01:59:38 +0200

    Added a package-info file explaining the contents of the main sort algorithm packages.

---
+++ src/main/java/com/bgasparotto/datastructures/algorithm/sort/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package containing the reference implementation of the sort algorithms provided by Udemy's course
+ * <a href="https://www.udemy.com/course/data-structures-and-algorithms-deep-dive-using-java">
+ * Data Structures and Algorithms: Deep Dive Using Java
+ * </a>
+ */
+package com.bgasparotto.datastructures.algorithm.sort;
